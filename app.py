eb349ea8a3bbb8ee16a9ee4d7345bfde1cb50538
PebblesII <shobell70@gmail.com>
2024-11-19 09:04:27 -0300

    h

--- app.py
+++ app.py
@@ -6,3 +6,14 @@ app = Flask(__name__, template_folder = "siteciencia/templates", static_folder= 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/analise", defaults={"usuario":"visitante"})
+@app.route("/analise/<usuario>")
+def analise(usuario):
+    return render_template("analise.html", usuario=usuario)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
